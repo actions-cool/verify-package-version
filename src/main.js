22fc18b1ed7ce2b25e17242f3bb42ce6972917df
xrkffgg <xrkffgg@vip.qq.com>
2021-02-03 13:30:48 +0800

    wip

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
 const core = require('@actions/core');
+const { Octokit } = require('@octokit/rest');
+const github = require('@actions/github');
 
+// **********************************************************
+const token = core.getInput('token') || '';
+const octokit = new Octokit({ auth: `token ${token}` });
+const context = github.context;
+
+// **********************************************************
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString());
-    await new Promise(resolve => {
-      setTimeout(() => resolve('done!'), 10);
-    });
-    core.debug(new Date().toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const { owner, repo } = context.repo;
+    if (context.eventName === 'pull_request') {
+      const PRTitle = context.payload.pull_request.title;
+      console.log(context.payload.pull_request.head)
+    } else {
+      core.info("This is now support 'pull_request'. I haven't thought about other trigger verification conditions for the time being, you can propose them!")
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
+// **********************************************************
 run();
